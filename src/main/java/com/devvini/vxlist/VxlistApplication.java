67bdb8bbe7c0d258599b3129a83446538caf45c8
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-05-13 18:51:13 -0300

    Projeto criado

---
+++ src/main/java/com/devvini/vxlist/VxlistApplication.java
@@ -0,0 +1,13 @@
+package com.devvini.vxlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VxlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VxlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devvini/vxlist/VxlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devvini.vxlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VxlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
